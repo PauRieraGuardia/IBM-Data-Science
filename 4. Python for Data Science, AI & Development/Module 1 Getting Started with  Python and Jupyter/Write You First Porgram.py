75535bc178b3e77669bd7a5918df0eb77b5ba402
PauRieraGuardia <159650154+PauRieraGuardia@users.noreply.github.com>
2024-06-22 18:09:06 +0200

    Add files via upload

---
+++ 4. Python for Data Science, AI & Development/Module 1 Getting Started with  Python and Jupyter/Write You First Porgram.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Try your first Python ouput
+print("Hello Python")
+
+
+# In[2]:
+
+
+# Check the Python Version
+import sys
+print(sys.version)
+
+
+# In[3]:
+
+
+# Practice on writing comment
+print ("Hello Wolrd")#This line prints a string
+
+
+# ## Errors in Python
+# 
+# Python will tell you that you have made a mistake by giving you an error message.
+
+# In[4]:
+
+
+# Print string as error message
+frint ("Hello World")
+
+
+# The error message tells you: 
+# 1. wher the error ocurred (more useful in large notebook cells or scripts) 
+# 2. what kind of error it was (NameError)
+
+# ## Exercise: Your First Program
+# 
+# In the code below, uste the print() function to print out the phrase: Hello World
+
+# In[6]:
+
+
+# Write your code below. Don' forget to press Shift+Enter to execute the cell
+print("Hello, World!")
+
+
+# In[8]:
+
+
+print("Hello, World!") #Print the traditional hello world
+
+
+# In[13]:
+
+
+x=5
+y=7
+z=x+y
+print(z)
+
